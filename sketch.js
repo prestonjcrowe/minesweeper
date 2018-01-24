277964051ef42c5ddef8d1d376864a0bef9f72f3
Preston Crowe <prestonjcrowe@gmail.com>
2018-01-23 17:14:16 -0800

    New flag / mine styles, gameOver state

--- sketch.js
+++ sketch.js
@@ -2,17 +2,21 @@ var gridSize = 20;
 var cellSize = 20;
 var grid = [];
 var mines = [];
-var startingMines = 35;
-var textHues = [198, 96, 0, 240, 0, 330, 26, 0]; // for 8: s:31, b:50
+var startingMines = 50;
 var tileColor;
 var gameOver;
 var f = false;
 
 function setup() {
-  createCanvas(400, 400);
-  colorMode(HSB);
-  tileColor = color(0, 0, 80);
-  gameOver = true;
+  createCanvas(windowWidth, windowWidth);
+  const NUM_COLORS = [
+                [99, 177, 240], [176, 214, 139],
+  				[213, 96, 97], [174, 137, 213],
+  				[253, 166, 87], [145, 203, 216],
+  				[255, 255, 255], [137, 158, 156]];
+  tileColor = color(47, 49, 48);
+
+  gameOver = false;
   for (var n = 0; n < gridSize; n++) {
     grid[n] = [];
   }
@@ -23,18 +27,17 @@ function setup() {
         y: j,
         hidden: "true",
         state: 0,
-        flagged: "false",
+        flagged: false,
         show: function() {
-          fill(0, 0, 50);
+          fill(27, 29, 28);
           rect(cellSize * this.x, cellSize * this.y, cellSize, cellSize);
           textSize(15);
           fill(0);
           textAlign(CENTER, CENTER);
-          if (this.state != 8) {
-            fill(textHues[this.state - 1], 100, 100);
-          } else {
-            fill(textHues[this.state - 1], 30, 50);
-          }
+          if (this.state> 0) {
+            var c = color(NUM_COLORS[this.state - 1][0], NUM_COLORS[this.state - 1][1], NUM_COLORS[this.state - 1][2]);
+            fill(c);
+          } 
           if (this.state != 0 && this.state != -9) {
             text(this.state, this.x * cellSize + cellSize / 2, this.y * cellSize + cellSize / 2);
           } else if (this.state == -9) {
@@ -53,7 +56,6 @@ function setup() {
           if (this.state == 0) {
             for (var r = this.x - 1; r < this.x + 2; r++) {
               for (var t = this.y - 1; t < this.y + 2; t++) {
-                //println("x: " + r + ", y: " + t);
                 if (r > -1 && r < gridSize &&
                   t > -1 && t < gridSize && grid[r][t].hidden) {
                   grid[r][t].reveal();
@@ -83,7 +85,7 @@ var placeMines = function() {
   for (var p = 0; p < startingMines; p++) {
     var rand1 = Math.round(random(gridSize - 1));
     var rand2 = Math.round(random(gridSize - 1));
-    console.log("Mine " + p + " Location: " + rand1 + " , " + rand2 + ".");
+    //console.log("Mine " + p + " Location: " + rand1 + " , " + rand2 + ".");
     if (grid[rand1][rand2].state != -9) {
       grid[rand1][rand2].state = -9;
       mines[p] = grid[rand1][rand2];
@@ -101,60 +103,55 @@ var placeMines = function() {
   }
 };
 
+var revealAll = function() {
+    for (var i = 0; i < gridSize; i++) {
+        for (var j = 0; j < gridSize; j++) {
+            grid[i][j].reveal();
+        }
+    }
+}
+
 var drawMine = function(x, y) {
-  fill(0, 0, 0);
-  line(cellSize / 2 + (cellSize * x), cellSize / 2 + (cellSize * y) - (cellSize / 2.5), cellSize / 2 + (cellSize * x), cellSize / 2 + (cellSize * y) + (cellSize / 2.5));
-  line(cellSize / 2 + (cellSize * x) - (cellSize / 2.5), cellSize / 2 + (cellSize * y), cellSize / 2 + (cellSize * x) + (cellSize / 2.5), cellSize / 2 + (cellSize * y));
-  ellipse(cellSize / 2 + (cellSize * x), cellSize / 2 + (cellSize * y), cellSize - 10, cellSize - 10);
-  fill(0, 0, 100);
-  ellipse(cellSize / 2 + (cellSize * x) - 3, cellSize / 2 + (cellSize * y) - 3, 3, 3);
+    fill(244, 86, 66);
+    rect(x * cellSize, y * cellSize, cellSize, cellSize);
 };
 
 var drawFlag = function(x, y) {
-  strokeWeight(3);
-  line(cellSize / 2 + (cellSize * x), cellSize / 2 + (cellSize * y) - (cellSize / 2.5), cellSize / 2 + (cellSize * x), cellSize / 2 + (cellSize * y) + (cellSize / 2.5));
-  strokeWeight(1);
-  fill(0, 100, 100);
-  triangle(cellSize / 2 + (cellSize * x), cellSize / 2 + (cellSize * y) - (cellSize / 2.5), cellSize / 2 + (cellSize * x), cellSize / 2 + (cellSize * y), cellSize / 2 + (cellSize * x) + (cellSize / 4), cellSize / 2 + (cellSize * y) - (cellSize / 4));
-
+    fill(158, 229, 91);
+    ellipse(x * cellSize + cellSize / 2, y * cellSize + cellSize / 2,
+        cellSize / 3, cellSize / 3);
 };
 
 var keyPressed = function() {
-  if (key == 'f' || key == 'F') {
-    var xpos = Math.floor(mouseX / cellSize);
-    var ypos = Math.floor(mouseY / cellSize);
-    println(xpos + " " + ypos);
-    f = true;
-    if(grid[xpos][ypos].flagged == false){
-      grid[xpos][ypos].flagged = true;
+    if (keyCode === SHIFT) {
+        f = true;
     }
-    else {
-      grid[xpos][ypos].flagged = false;
+    if (key === ' ') {
+        setup();
     }
-  }
 };
 
 var keyReleased = function() {
-  if (key == 'f' || key == 'F') {
+  if (keyCode === SHIFT) {
     f = false;
   }
 };
 
 var mousePressed = function() {
-  var xpos = Math.floor(mouseX / cellSize);
-  var ypos = Math.floor(mouseY / cellSize);
+    if (!gameOver) {
+        var xpos = Math.floor(mouseX / cellSize);
+        var ypos = Math.floor(mouseY / cellSize);
   
-  println(xpos + " " + ypos);
-  
-  if (!f) {
-    if (grid[xpos][ypos].flagged) {
-      grid[xpos][ypos].reveal();
+        if (f === true) {
+            grid[xpos][ypos].flagged = !grid[xpos][ypos].flagged; 
+        } else {
+            if (!grid[xpos][ypos].flagged) {
+                grid[xpos][ypos].reveal();
+                if (mines.includes(grid[xpos][ypos])) {
+                    gameOver = true;
+                    //revealAll();
+                }
+            }
+        }
     }
-    println("revealed");
-  } else if (f) {
-    if(!grid[xpos][ypos].flagged){
-      grid[xpos][ypos].flagged = false;
-    }
-    println("flagthisshit");
-  }
-};
+ };
